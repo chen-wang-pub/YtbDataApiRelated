8c97f79056fb9583d40ba570af6b2e6126bdb3f5
WommyInStandingPosition <woomyintheend@gmail.com>
2021-08-31 02:37:23 -0700

    update the objective queue

--- todo_lsit.py
+++ todo_lsit.py
@@ -6,5 +6,7 @@ objective_queue = [
     'Optional: try deploy a scrapy project in docker',
     'Deploy a flask server that makes use of pytube, download audio & video upon request and transfer it to the client',
     'Try deploy a format convertor that converts webm format to mp3 in docker',
-    'Deploy a server that takes a webm file, convert it to mp3 then send it back'
+    'Deploy a server that takes a webm file, convert it to mp3 then send it back',
+    'Learn python message queue',
+    'Refactor part of or the whole project using message queue',
 ]
