8d78c57c03b351658c9479af54ab9b79b57a80e1
WommyInStandingPosition <woomyintheend@gmail.com>
2021-08-31 02:20:48 -0700

    future objectives

---
+++ todo_lsit.py
@@ -0,0 +1,10 @@
+objective_queue = [
+    'Learn how to use selenium webdrive with python in docker',
+    'Collect proxy info from https://sslproxies.org/ via selenium in docker',
+    'Run a web scraping script using python requests in docker',
+    'Deploy a flask server that takes in web-scraping scripts, stores in db, executes them based on requests',
+    'Optional: try deploy a scrapy project in docker',
+    'Deploy a flask server that makes use of pytube, download audio & video upon request and transfer it to the client',
+    'Try deploy a format convertor that converts webm format to mp3 in docker',
+    'Deploy a server that takes a webm file, convert it to mp3 then send it back'
+]
