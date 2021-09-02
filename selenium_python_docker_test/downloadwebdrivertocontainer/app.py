828849abd67526069fdceec00d1e84340bbd196b
WommyInStandingPosition <woomyintheend@gmail.com>
2021-09-02 02:40:46 -0700

    Got an example of using python & chrome webdriver in the same container working
    Based on the two attempts, it seems using a standalone webdriver container with the app container together using docker compose is the better choice

--- selenium_python_docker_test/downloadwebdrivertocontainer/app.py
+++ selenium_python_docker_test/downloadwebdrivertocontainer/app.py
@@ -3,16 +3,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
 
 
 if __name__ == "__main__":
-    firefox_profile = webdriver.FirefoxProfile()
-    firefox_profile.set_preference('browser.download.folderList', 2)
-    firefox_profile.set_preference('browser.download.manager.showWhenStarting', False)
-    firefox_profile.set_preference('browser.download.dir', os.getcwd())
-    firefox_profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
-    driver = webdriver.Firefox(firefox_profile=firefox_profile)
+    from selenium import webdriver
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("window-size=1400,2100")
+    chrome_options.add_argument('--disable-gpu')
+
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get("https://sslproxies.org/")
     ips = [my_elem.get_attribute("innerHTML") for my_elem in WebDriverWait(driver, 5).until(
         EC.visibility_of_all_elements_located((By.XPATH,
