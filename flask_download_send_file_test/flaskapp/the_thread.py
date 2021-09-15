d4a35e48ab504a11a798e430cddb19d20c40ed12
WommyInStandingPosition <woomyintheend@gmail.com>
2021-09-15 09:39:26 -0700

    Tested how the download thread should be implemented & the on_progress on_complete callback
    Need to complete the on_complete function to update the status in DB next
    Need to add certain restriction to request of downloading large size files as the timeout parameter is not for the total elapsed download time

--- flask_download_send_file_test/flaskapp/the_thread.py
+++ flask_download_send_file_test/flaskapp/the_thread.py
@@ -5,6 +5,7 @@ import json
 import requests
 
 from flask import current_app
+from pytube import YouTube
 
 class TheThread(threading.Thread):
     """
@@ -102,6 +103,53 @@ class PytubeThread(threading.Thread):
 
 
 if __name__ == '__main__':
+    itemid_l = 'LHhLcvmQbx8'
+    itemid_s = 'Gu3IOnDQzko'
+    base_url = 'https://www.youtube.com/watch?v='
+    timeout = 10
+
+    temp_dir_loc = os.path.join(os.path.dirname(__file__), 'temp_storage')
+    folder_name = 'yerw'
+
+    dirlocation = os.path.join(temp_dir_loc, folder_name)
+
+    download_url = '{}{}'.format(base_url, itemid_l)
+
+
+    def on_progress(stream, chunk,  bytes_remaining):
+        """On download progress callback function.
+        :param object stream:
+            An instance of :class:`Stream <Stream>` being downloaded.
+        :param file_handle:
+            The file handle where the media is being written to.
+
+        :param int bytes_remaining:
+            How many bytes have been downloaded.
+        """
+        filesize = stream.filesize
+        bytes_received = filesize - bytes_remaining
+        print(bytes_received/filesize*100)
+    def on_complete(stream, file_handle):
+        """
+
+        :param file_handle:
+        :return:
+        """
+        print(stream)
+        print(file_handle)
+        print('completed')
+    def download_best_audio(url, output_path, filename):
+        yt = YouTube(url, on_progress_callback=on_progress, on_complete_callback=on_complete)
+        print('about to download {} from url: {}'.format(yt.title, url))
+        all_stream = yt.streams.filter(only_audio=True)
+        #print(all_stream)
+        best_quality = all_stream[-1]  # last in list
+        #print(best_quality)
+        stream = yt.streams.get_by_itag(best_quality.itag)
+        return stream.download(output_path=output_path, timeout=timeout, filename=filename)
+
+    print(download_best_audio(download_url, dirlocation, itemid_s))
+
     """db_info_dict = {
         'db_url': '172.17.0.4',
         'db_port': '27017',
