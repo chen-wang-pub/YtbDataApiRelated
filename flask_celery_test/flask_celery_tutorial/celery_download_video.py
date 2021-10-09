55837cf187ba7ae553ecda41c8f03f8663afb9ee
WommyInStandingPosition <woomyintheend@gmail.com>
2021-10-09 13:15:32 -0700

    tested download video using celery task

--- flask_celery_test/flask_celery_tutorial/celery_download_video.py
+++ flask_celery_test/flask_celery_tutorial/celery_download_video.py
@@ -1,5 +1,5 @@
 from celery import Celery
-from flask import Flask, request
+from flask import Flask, request, Response, json
 from pytube import YouTube
 
 
@@ -19,18 +19,28 @@ def make_celery(app):
     celery.Task = ContextTask
     return celery
 
-flask_app = Flask(__name__)
+flask_app = Flask('celery_download_video')
 flask_app.config.update(
     CELERY_BROKER_URL='redis://localhost:6379',
     CELERY_RESULT_BACKEND='redis://localhost:6379'
 )
 celery = make_celery(flask_app)
 
+
+def on_complete(stream, file_handle):
+    """
+
+    :param file_handle:
+    :return:
+    """
+    flask_app.logger.debug('{} download finished'.format(file_handle))
+
+
 @celery.task
 def download_video(ytb_id):
     ytb_base_url = 'https://www.youtube.com/watch?v='
     download_url = '{}{}'.format(ytb_base_url, ytb_id)
-    yt = YouTube(download_url)
+    yt = YouTube(download_url, on_complete_callback=on_complete)
     all_stream = yt.streams.filter(only_audio=True)
     best_quality = all_stream[-1]  # last in list
     stream = yt.streams.get_by_itag(best_quality.itag)
@@ -41,3 +51,15 @@ def download_video(ytb_id):
 def queue_download():
     video_ids = request.args.get('id', default='', type=str)
     video_ids = video_ids.split(',')
+    for video_id in video_ids:
+        download_video.delay(video_id)
+        flask_app.logger.debug('{} is queued for downloading'.format(video_id))
+    return Response(response=json.dumps({"Succeeded": "Download for {} is queued. "
+                                                      "Please use "
+                                                      "/ytbaudiodownload/v0/downloadbyvideoid/{} "
+                                                      "for checking status and "
+                                                      "retrieving the file"}),
+                    status=200,
+                    mimetype='application/json')
+if __name__ == '__main__':
+    flask_app.run(debug=True)
