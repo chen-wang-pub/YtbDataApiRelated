95b3ec3653356d8cd31c55dbc25b4656571fb5c6
WommyInStandingPosition <woomyintheend@gmail.com>
2021-10-24 22:09:37 -0700

    flask form tutorial

--- flask_celery_test/app_test/app/factory.py
+++ flask_celery_test/app_test/app/factory.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import os
 from .celery_utils import init_celery
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 PKG_NAME = os.path.dirname(os.path.realpath(__file__)).split("/")[-1]
 
@@ -9,5 +11,14 @@ def create_app(app_name=PKG_NAME, **kwargs):
     if kwargs.get("celery"):
         init_celery(kwargs.get("celery"), app)
     from app.all import bp
+    from app.paid_user import paid_user
+    from app.config import Config
     app.register_blueprint(bp)
+    app.register_blueprint(paid_user)
+
+    app.config.from_object(Config)
+    #db = SQLAlchemy(app)
+    #migrate = Migrate(app, db)
+
+    #from app import models
     return app
